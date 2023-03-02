1f3c2e8fc2ff18989925259685bc5c6cca44d80f
Laura Gabriele <lauragabriele2015@gmail.com>
2023-03-02 13:16:31 -0300

    algumas alterações foram realizadas

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const res = document.getElementById('res')//div res do html, para colocar frase na tela 
-const repete = document.getElementById('repete')//div repete do html, para colocar a frase dos números que já foram inseridos e que estão incorretos
-var numbersSelect = []//definindo o numbersSelect que é um array que vai ser utilizado para guardar os valores que foram selecionados e que estão incorretos
+const response = document.getElementById('response')
+const incorrectNumbers = document.getElementById('incorrectNumbers')
+var numbersSelect = []
 const select = document.querySelector('.number')
-
 function getSelectValue() {
-  const valorSelect = select.value//pegando o valorSelect do select do html
-  return valorSelect//dando retorno para poder utilizar fora da função
+  const valorSelect = select.value
+  console.log(valorSelect)
+  return valorSelect
 }
-function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min) + min)
-}
-
 arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const numeroAleatorio = getRandomInt(arrayNumeros[0], arrayNumeros[9])
+const numeroAleatorio = Math.floor(Math.random() * 10)
 console.log(numeroAleatorio)
 
 function options(array) {
-  while (select.firstChild){
+  while (select.firstChild) {
     select.removeChild(select.firstChild)
   }
   for (i = 0; i < array.length; i++) {
@@ -30,31 +24,30 @@ function options(array) {
   }
 }
 options(arrayNumeros)
-
-const compareNumber = function () {//VARIAVEL QUE RECEBE UMA FUNÇÃO
+const compareNumber = function () {
   const valorSelecionado=getSelectValue()
   numbersSelect.push(valorSelecionado)
   if (numbersSelect[numbersSelect.length - 1] == numeroAleatorio) {
-    res.innerText = ("Deu certo")
+    response.innerText = ("Deu certo")
     document.body.style.background = "green"
-    repete.innerText=""
+    incorrectNumbers.innerText = ""
   }
   const index = arrayNumeros.indexOf(parseInt(valorSelecionado))
   arrayNumeros.splice(index, 1)
   while (numbersSelect[numbersSelect.length - 1] !== numeroAleatorio) {
-    
+
     if (numbersSelect[numbersSelect.length - 1] > numeroAleatorio) {
-      res.innerText = "Você errou! O número selecionado é MAIOR que o número sorteado."
+      response.innerText = "Você errou! O número selecionado é MAIOR que o número sorteado."
       document.body.style.background = "red"
-      repete.innerText = `Os seguintes números já foram inseridos: ${numbersSelect}. Tente com outro número!`
-     
+      incorrectNumbers.innerText = `Os seguintes números já foram inseridos: ${numbersSelect}. Tente com outro número!`
+
     }
     if (numbersSelect[numbersSelect.length - 1] < numeroAleatorio) {
-      res.innerText = "Você errou! O número selecionado é MENOR que o número sorteado."
+      response.innerText = "Você errou! O número selecionado é MENOR que o número sorteado."
       document.body.style.background = "red"
-      repete.innerText = `Os seguintes números já foram inseridos: ${numbersSelect}. Tente com outro número!`
+      incorrectNumbers.innerText = `Os seguintes números já foram inseridos: ${numbersSelect}. Tente com outro número!`
 
-      
+
     }
     options(arrayNumeros)
     break
